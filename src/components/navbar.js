80e7ace383ef72cc8d680569ad518ec4ab0ef125
thekarandeveloper <thekarankumar.dev@gmail.com>
2024-01-27 22:56:38 +0530

    base-responsive-completed

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -3,6 +3,8 @@ import { Link } from "react-router-dom";
 import { MdFileDownload } from "react-icons/md";
 import { IoIosMenu } from "react-icons/io";
 import { IoCloseOutline } from "react-icons/io5";
+import { AiFillHome } from "react-icons/ai";
+import { BiSolidFilePdf } from "react-icons/bi";
 function Navbar() {
   const navRef = useRef();
   const btnRef = useRef();
@@ -11,6 +13,7 @@ function Navbar() {
     navRef.current.classList.toggle("responsive_nav");
     setIsClosed((prevIsClosed) => !prevIsClosed);
   };
+  const resumeFilePath = process.env.PUBLIC_URL + '/KARAN_KUMAR_RESUME.pdf';
 
   return (
     <header>
@@ -20,26 +23,31 @@ function Navbar() {
         </div>
         <div className="nav-items" ref={navRef}>
           <Link className="label nav-item active" to="/">
-            Home
+            <div className="nav-item-icon heading">
+              <AiFillHome />
+            </div>
+            <div className="nav-item-label">Home</div>
           </Link>
-          {/* <Link className="label nav-item" to="/projects">
-            Projects
-          </Link>
-          <Link className="label nav-item" to="/">
-            Experince
-          </Link> */}
 
-          <a className="label nav cta-btn" href="../assets/KARAN_KUMAR_RESUME.pdf" download={true}>
+          <a
+            className="label nav cta-btn"
+            href={resumeFilePath}
+            download={true}
+          >
             Resume{" "}
             <span>
               <MdFileDownload />
             </span>
           </a>
         </div>
-        <div className="nav-toggle" ref={btnRef} onClick={showNavbar}>
+        <a
+          className="nav-toggle"
+          download={true}
+          href={resumeFilePath}
+        >
           {" "}
-          {isClosed ? <IoIosMenu /> : <IoCloseOutline />}
-        </div>
+          <BiSolidFilePdf />
+        </a>
       </navbar>
     </header>
   );
