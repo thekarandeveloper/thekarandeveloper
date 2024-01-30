cb1116a888e521a986e91dfafa8776e8920d6679
thekarandeveloper <thekarankumar.dev@gmail.com>
2024-01-31 01:13:08 +0530

    usernames-updated

--- src/components/infoCard.js
+++ src/components/infoCard.js
@@ -17,15 +17,15 @@ function InfoCard() {
           <div><IoMail /></div>
           <div className="icon-label"><span>Mail Me</span></div>
         </a>
-        <a className="icon-container label" target="_blank" href="https://twitter.com/karan_developer">
+        <a className="icon-container label" target="_blank" href="https://twitter.com/karankumarcv">
           <div><FaXTwitter /></div>
           <div className="icon-label"><span>Thoughts</span></div>
         </a>
-        <a className="icon-container label" target="_blank" href="https://www.linkedin.com/in/thekarandeveloper/">
+        <a className="icon-container label" target="_blank" href="https://www.linkedin.com/in/karankumarcv/">
           <div><IoLogoLinkedin /></div>
           <div className="icon-label"><span>Work Life</span></div>
         </a>
-        <a className="icon-container label" target="_blank" href="https://www.instagram.com/karandeveloper/">
+        <a className="icon-container label" target="_blank" href="https://www.instagram.com/karankumarcv/">
           <div><RiInstagramFill /></div>
           <div className="icon-label"><span>Social Life</span></div>
         </a>
