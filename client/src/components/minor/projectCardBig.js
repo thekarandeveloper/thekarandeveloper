bde943edfc7c4804f01b7ca9c74a00a36e517169
Karan Kumar <thekarankumar.dev@gmail.com>
2024-08-19 18:13:56 +0530

    mockups-updated

--- client/src/components/minor/projectCardBig.js
+++ client/src/components/minor/projectCardBig.js
@@ -12,19 +12,19 @@ function projectCardBig({
 }) {
   return (
     <div
-      className="w-full h-fit bg-body flex flex-col justify-center items-center p-4 cursor-pointer md:col-span-2  gap-x-4 lg:h-full"
+      className="w-full h-fit bg-body flex flex-col justify-center items-center p-4 cursor-pointer md:col-span-2  gap-x-4 lg:h-full md:flex-row"
       onClick={subToggle}
     >
       <div
-        className="w-full h-[25vh] rounded-large bg-no-repeat bg-cover mb-6  lg:mb-0 lg:h-full "
-        style={{ backgroundImage: `url(${imgname})` }}
+        className="w-full h-[25vh] rounded-large bg-no-repeat bg-contain mb-6  lg:mb-0 lg:h-[70%] mt-4 md:w-[40%]"
+        style={{ backgroundImage: `url(${imgname})`}}
       ></div>
-      <div className="w-full h-full flex flex-col items-start justify-center p-2 gap-y-6 pt-6">
-        <div className="title text-center w-full lg:text-left ">{title}</div>
-        <div className="big-card-desc body-normal opacity-60 text-center lg:text-left">
+      <div className="w-full h-full flex flex-col items-start justify-center p-2 gap-y-6 pt-6 md:w-[60%]">
+        <div className="title text-center w-full md:text-left ">{title}</div>
+        <div className="big-card-desc body-normal opacity-60 text-center md:text-left">
           {desc}
         </div>
-        <div className="big-card-tags w-full hidden justify-center flex-wrap gap-y-2 gap-x-3 opacity-70 md:flex lg:justify-start ">
+        <div className="big-card-tags w-full hidden justify-center flex-wrap gap-y-2 gap-x-3 opacity-70 md:flex md:justify-start ">
           <div className="big-card-tag-item body-normal bg-light-bg p-3">{tag1}</div>
           <div className="big-card-tag-item body-normal  bg-light-bg p-3">{tag2}</div>
           <div className="big-card-tag-item body-normal  bg-light-bg p-3">{tag3}</div>
--- client/src/components/minor/projectCardSmall.js
+++ client/src/components/minor/projectCardSmall.js
@@ -8,7 +8,7 @@ function projectCardSmall({
 }) {
   return (
     <div className="project-small-card-container  flex flex-col justify-start items-center p-5 cursor-pointer bg-body lg:min-h-[30vw]" onClick={subToggle}>
-      <div className="project-small-card-img w-full min-h-[25vh] lg:h-full overflow-hidden rounded-lg" style={{backgroundImage:`url(${imgname})`}}>
+      <div className="project-small-card-img w-full min-h-[25vh] lg:h-full overflow-hidden rounded-lg bg-no-repeat bg-contain my-4" style={{backgroundImage:`url(${imgname})`}}>
       </div>
       <div className="project-small-card-body w-full h-full flex flex-col items-center justify-center p-5 gap-y-3 lg:gap-y-0">
         <div className="project-small-title title pt-2 lg:pb-4">{title}</div>
