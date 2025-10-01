ce2cf56c46b1fa5e92e30ec7531ff3f863cf4cf0
Karan Kumar <thekarankumar.dev@gmail.com>
2025-10-01 16:49:45 +0530

    initiated-top-apps-banner

--- src/app/layout.js
+++ src/app/layout.js
@@ -3,6 +3,7 @@ import Navbar from "../../components/Navbar"
 import HeroSection from "../../components/HeroSection"
 import IphoneCarousel from "../../components/micro/carousel"
 import TextMetrics from "../../components/metrics"
+import BannerApps from "../../components/TopAppsBanner"
 export const metadata = {
   title: "Karan’s Portfolio",
   description: "iOS Developer Portfolio",
@@ -16,6 +17,7 @@ export default function RootLayout({ children }) {
       <HeroSection />
       <IphoneCarousel/>
       <TextMetrics></TextMetrics>
+      <BannerApps></BannerApps>
       </body>
     </html>
   )
