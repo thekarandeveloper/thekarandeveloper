a82f1a9594de192ad3800c643fd042ea8d060a57
Karan Kumar <thekarankumar.dev@gmail.com>
2025-10-01 23:22:16 +0530

    fixed-top-apps

--- components/micro/TopApps.jsx
+++ components/micro/TopApps.jsx
@@ -26,14 +26,33 @@ export default function TopApps({
       </div>
 
       {/* Content */}
-      <div className={`flex flex-col gap-2 w-full md:w-1/2 pr-0 ${imagePosition === "right" ? "md:pl-20" : "md:pr-20"} text-center md:text-left`}>
+      <div className={`flex flex-col gap-3 w-full md:w-1/2 pr-0 ${imagePosition === "right" ? "md:pl-20" : "md:pr-20"} text-center md:text-left`}>
         
         {name && <div className="text-md font-bold">{name}</div>}
 
         {desc && <div className="text-2xl font-light">{desc}</div>}
 
+        {/* Metrics */}
+        
+        <div className="flex flex-row gap-4 w-full border-b-1 pb-4 border-b-gray-700">
+ <div className="flex flex-col w-30 p-5 gap-4 rounded-md bg-[#212121]">
+            <div className="text-xs font-light">Installs</div>
+            <div className="text-3xl font-bold">30+</div>
+        </div>
+         <div className="flex flex-col w-30 p-5 gap-4 rounded-md bg-[#212121]">
+            <div className="text-xs font-light">Installs</div>
+            <div className="text-3xl font-bold">30+</div>
+        </div>
+         <div className="flex flex-col w-30 p-5 gap-4 rounded-md bg-[#212121]">
+            <div className="text-xs font-light">Installs</div>
+            <div className="text-3xl font-bold">30+</div>
+        </div>
+        </div>
+       
+
+
         {/* CTA Buttons */}
-        <div className="flex justify-center md:justify-start gap-4 mt-4">
+        <div className="flex justify-center md:justify-start gap-4 mt-2">
           {appStoreLink && (
              <ActionButton label={"App Store"} Icon={ArrowDownToLineIcon} color="blue"  onClick={() => alert("Grey clicked!")}></ActionButton>
 
@@ -41,7 +60,7 @@ export default function TopApps({
 
           {caseStudyLink && (
 
-               <ActionButton label={"Case Study"} Icon={ArrowDownToLineIcon} color="blue"  onClick={() => alert("Grey clicked!")}></ActionButton>
+               <ActionButton label={"Case Study"} Icon={ArrowDownToLineIcon} color="white"  onClick={() => alert("Grey clicked!")}></ActionButton>
 
           
           )}
