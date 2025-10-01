6773311611de1f63b629e600d812f5b389953c59
Karan Kumar <thekarankumar.dev@gmail.com>
2025-10-01 20:32:30 +0530

    fixed-metrics

---
+++ components/micro/Metrics.jsx
@@ -0,0 +1,48 @@
+"use client"
+
+import { useEffect, useState, useRef } from "react"
+
+export default function MetricCard({ number, desc, duration = 2000 }) {
+  const [count, setCount] = useState(0)
+  const [hasAnimated, setHasAnimated] = useState(false)
+  const ref = useRef(null)
+  const start = useRef(null)
+
+  useEffect(() => {
+    const observer = new IntersectionObserver(
+      ([entry]) => {
+        if (entry.isIntersecting && !hasAnimated) {
+          setHasAnimated(true)
+          requestAnimationFrame(animate)
+        }
+      },
+      { threshold: 0.5 } // trigger when 50% visible
+    )
+
+    if (ref.current) observer.observe(ref.current)
+
+    return () => {
+      if (ref.current) observer.unobserve(ref.current)
+    }
+  }, [hasAnimated])
+
+  const animate = (timestamp) => {
+    if (!start.current) start.current = timestamp
+    const progress = timestamp - start.current
+    const progressRatio = Math.min(progress / duration, 1)
+    setCount(Math.floor(progressRatio * number))
+
+    if (progress < duration) {
+      requestAnimationFrame(animate)
+    } else {
+      setCount(number)
+    }
+  }
+
+  return (
+    <div ref={ref} className="flex flex-col gap-4 text-center py-10 px-20">
+      <div className="text-[55px] font-bold">+{count}</div>
+      <div className="text-lg w-70 font-light">{desc}</div>
+    </div>
+  )
+}
