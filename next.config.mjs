bd86fcae7d2cced1af201c85c7a57fc4a696e6d8
Karan Kumar <thekarankumar.dev@gmail.com>
2025-10-03 02:34:25 +0530

    fixed-redicrects

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,19 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    async redirects() {
+    return [
+      {
+        source: '/github',   
+        destination: '/https://github.com/thekarandeveloper', 
+        permanent: true,       // true -> 301 redirect, false -> 302 temporary
+      },
+      {
+        source: '/linkedin',
+        destination: 'https://www.linkedin.com/in/karandeveloper/',
+        permanent: true,
+      },
+    ]
+  },
+};
 
 export default nextConfig;
