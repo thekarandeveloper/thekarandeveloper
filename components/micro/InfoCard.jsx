1f043dc13f2ddae7286be04067242eebe3f63ac5
Karan Kumar <thekarankumar.dev@gmail.com>
2025-10-02 16:23:02 +0530

    fixed-infocard

--- components/micro/InfoCard.jsx
+++ components/micro/InfoCard.jsx
@@ -35,11 +35,12 @@ export default function InfoCard({
 
   return (
     <div
-      className={`min-w-75 max-w-75 h-85 flex-shrink-0 flex flex-col bg-[#272727] rounded-3xl overflow-hidden ${className}`}
+      className={`min-w-[250px] max-w-[250px] bg-[#272727] rounded-3xl overflow-hidden flex-shrink-0 flex flex-col ${className}
+        w-full md:min-w-[300px] md:max-w-[300px] h-[360px]`}
       style={{ width: width === "full" ? "100%" : width }}
     >
       {/* Top: Image or Placeholder */}
-      <div className="min-h-[50%] flex-1 flex items-center justify-center bg-gray-200 relative">
+      <div className="h-1/2 flex items-center justify-center bg-gray-200 relative">
         {imageSrc ? (
           <img
             src={imageSrc}
@@ -52,14 +53,14 @@ export default function InfoCard({
       </div>
 
       {/* Bottom: Content */}
-      <div className="h-[50%] gap-2 flex-1 p-6 flex flex-col justify-start items-start text-left text-white">
+      <div className="h-1/2 p-6 flex flex-col justify-start items-start text-left text-white">
         {heading && (
-          <h3 className="flex items-start justify-start py-0 text-xl md:text-xl font-medium">
+          <h3 className="text-md md:text-xl font-medium mb-2">
             {heading}
           </h3>
         )}
         {description && (
-          <p ref={ref} className="text-base md:text-sm font-light">
+          <p ref={ref} className="text-sm md:text-sm font-light">
             {displayText}
           </p>
         )}
