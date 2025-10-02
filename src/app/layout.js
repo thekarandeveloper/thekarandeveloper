a3450f6218e5bf1a3bb880d41dc8f8a29b80deeb
Karan Kumar <thekarankumar.dev@gmail.com>
2025-10-03 01:41:43 +0530

    integerated-ga

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,31 +1,44 @@
 "use client";
 
-import "../../styles/globals.css"
-import Navbar from "../../components/Navbar"
-import Footer from "../../components/Footer"
-import { ModalProvider } from "../../context/ModalContext"
-import Modal from "../../components/Modal"
+import "../../styles/globals.css";
+import Navbar from "../../components/Navbar";
+import Footer from "../../components/Footer";
+import { ModalProvider } from "../../context/ModalContext";
+import Modal from "../../components/Modal";
 import MobileTabBar from "../../components/MobileTabBar";
-import { Briefcase, User, BookOpen } from "lucide-react";
+import { Briefcase, User } from "lucide-react";
 
-
+import { useEffect } from "react";
+import { usePathname } from "next/navigation";
+import { initGA, logPageView } from "../../lib/ga"; // import GA helper
 
 export default function RootLayout({ children }) {
   const tabs = [
     { href: "/", label: "Work", Icon: Briefcase },
     { href: "/about", label: "About", Icon: User },
   ];
+
+  const pathname = usePathname();
+
+  useEffect(() => {
+    initGA(); // Initialize GA once
+  }, []);
+
+  useEffect(() => {
+    logPageView(pathname); // Log each pageview
+  }, [pathname]);
+
   return (
     <html lang="en">
       <body className="bg-black text-white">
         <ModalProvider>
           <Navbar />
-          {children} {/* <- page specific content inject hoga */}
-           <MobileTabBar tabs={tabs} />
+          {children}
+          <MobileTabBar tabs={tabs} />
           <Footer />
           <Modal />
         </ModalProvider>
       </body>
     </html>
-  )
+  );
 }
