34a8b06a590f079703eb491d08c4d245e7b903d5
Karan Kumar <thekarankumar.dev@gmail.com>
2025-10-05 11:21:10 +0530

    fixed-resume-link

--- next.config.mjs
+++ next.config.mjs
@@ -14,7 +14,7 @@ const nextConfig = {
       },
       {
         source: '/resume',
-        destination: 'https://drive.google.com/file/d/1RtbtafuqySA1fmiqqaC6Ust2480T7sPb/view?usp=sharing',
+        destination: 'https://drive.google.com/file/d/1jtU-lzKuDveeA5oUfHZvrWMBo1l2BmsU/view?usp=sharing',
         permanent: true,
       },
       {
